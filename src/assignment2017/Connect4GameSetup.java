20562d83a5439a9115109e2d48a5f426402b7de6
Simon Fish <si@mon.fish>
2017-05-11 09:19:34 +0100

    Setup now outputs two players

--- src/assignment2017/Connect4GameSetup.java
+++ src/assignment2017/Connect4GameSetup.java
@@ -11,34 +11,56 @@ import javax.swing.*;
 
 import assignment2017.codeprovided.Connect4Player;
 
-public class Connect4GameSetup extends JFrame implements ActionListener {
+public class Connect4GameSetup extends JFrame {
 	public Container contentPane = getContentPane();
 	public final String[] playerOptions = { "Player", "CPU (Easy)" };
 	public JPanel gameSetupPanel = new JPanel(new GridLayout(0,1));
 	
-    JComboBox player1Selector = new JComboBox(playerOptions);
-    JComboBox player2Selector = new JComboBox(playerOptions);
-    
+    JComboBox selectorPlayer1 = new JComboBox(playerOptions);
+    JComboBox selectorPlayer2 = new JComboBox(playerOptions);
+    JButton buttonStart = new JButton("Start");
+    JButton buttonExit = new JButton("Exit");    
     Connect4Player r;
     Connect4Player y;
     Connect4Player[] players = new Connect4Player[2];
     
 	public Connect4GameSetup() {
 		// TODO Auto-generated constructor stub
-		player1Selector.setSelectedIndex(0);
-	    player2Selector.setSelectedIndex(1);
-	    player1Selector.addActionListener(this);
-	    gameSetupPanel.add(player1Selector);
-	    gameSetupPanel.add(player2Selector);
-	    gameSetupPanel.add(new JButton("Start"));
-	    gameSetupPanel.add(new JButton("Exit"));
+		players[0] = r;
+		players[1] = y;
+		selectorPlayer1.setSelectedIndex(0);
+		selectorPlayer2.setSelectedIndex(1);
+		buttonStart.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		int[] playerselection = new int[2];
+	    		Connect4Player[] playerslot = new Connect4Player[2];
+	    		playerselection[0] = selectorPlayer1.getSelectedIndex();
+	    		playerselection[1] = selectorPlayer2.getSelectedIndex();
+	    		for (int i = 0; i<playerselection.length; i++) {
+	    			switch (playerselection[i]) {
+	    			case 1:
+	    				playerslot[i] = new RandomPlayer();
+	    				break;
+	    			default:
+	    				playerslot[i] = new KeyboardPlayer();
+	    				break;
+	    			}
+	    		}
+	    		System.out.println(playerslot[0]);
+	    		System.out.println(playerslot[1]);
+	    	}
+	    });
+		buttonExit.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    		System.exit(0);
+	    	}
+	    });
+	    gameSetupPanel.add(selectorPlayer1);
+	    gameSetupPanel.add(selectorPlayer2);
+	    gameSetupPanel.add(buttonStart);
+	    gameSetupPanel.add(buttonExit);
 	    contentPane.add(gameSetupPanel);
 	    this.setVisible(true);
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		int player1 = player1Selector.getSelectedIndex();
-		System.out.println(player1);
 	}
 	
 	public static void main(String[] args) {
